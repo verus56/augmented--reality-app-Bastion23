f3ce80bed3f2c6b9e91ee2e685cadf3cba2e88c7
Haddad Sarah <haddads686@gmail.com>
2023-12-09 15:29:45 +0100

    I need to fix the UI

--- lib/Screens/getPosts.dart
+++ lib/Screens/getPosts.dart
@@ -1,5 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:like_button/like_button.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
 class getPosts extends StatefulWidget {
   final String documentID;
@@ -32,17 +37,126 @@ class _getPostsState extends State<getPosts> {
           Map<String, dynamic> data =
               snapshot.data!.data() as Map<String, dynamic>;
           return Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+            ),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 6,
+                    ),
+                    Expanded(
+                      child: InkWell(
+                        child: Container(
+                          margin: EdgeInsets.all(10),
+                          child: CachedNetworkImage(
+                            imageUrl: data["imageUrl"],
+                            imageBuilder: (context, imageProvider) => Container(
+                              width: 100.0,
+                              height: 100.0,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                    image: imageProvider, fit: BoxFit.cover),
+                              ),
+                            ),
+                            placeholder: (context, url) =>
+                                CircularProgressIndicator(),
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
+                          ),
+                        ),
+                        onDoubleTap: () {
+                          print("Double Tap .........");
+                          //LikePost(likes, DocumentId);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            bottomRight: Radius.circular(5),
+                            bottomLeft: Radius.circular(5)),
+                        color: Colors.grey.shade50,
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  height: 20,
+                                  margin: EdgeInsets.only(left: 10, top: 10),
+                                  child:Row(
+                                    children: [
+                                      InkWell(
+                                        child: LikeButton(
+                                          crossAxisAlignment: CrossAxisAlignment.center,
+                                                              mainAxisAlignment: MainAxisAlignment.center,
+                                                              countPostion: CountPostion.right,
+                                                              //likeCount:snapshot.data!.docs[index]['likes'],
+                                                              size: 30,
+                                        ),
+                                        onTap: () {
+                                          // await LikePost(likes,DocumentId);
+                                          print("Like");
+                                        },
+                                      ),
+                                       Padding(
+                                                          padding: EdgeInsets.only(left: 10,top: 2),
+                                                          child: Icon(Icons.comment,color: Colors.grey,size: 30,),
+                                                        ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  margin: EdgeInsets.only(left: 10),
+                                  child: Text(
+                                    data["caption"],
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )),
+                  ],
+                ),
                 //Text(data["TimeStamp"]),
                 //Text(data["UserId"]),
-                Image.network(
-                  data["imageUrl"],
-                  width: 100, // Set the width as needed
-                  height: 100, // Set the height as needed
-                  fit: BoxFit.cover, // Set the BoxFit as needed
-                ),
-                Text(data["caption"]),
                 //Text(data["likes"]),
               ],
             ),
